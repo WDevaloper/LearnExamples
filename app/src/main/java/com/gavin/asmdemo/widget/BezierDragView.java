3010edb9d3d510588edb4e44cc639b8bdaa6ff8f
f_y_wang6@163.com <f_y_wang6@163.com>
2019-12-26 00:10:41 +0800

    beizer

--- app/src/main/java/com/gavin/asmdemo/widget/BezierDragView.java
+++ app/src/main/java/com/gavin/asmdemo/widget/BezierDragView.java
@@ -2,28 +2,104 @@ package com.gavin.asmdemo.widget;
 
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
-
-import com.gavin.asmdemo.BaseCustomView;
+import android.view.View;
 
 
 /**
  * 消息气泡拖拽的 View
  */
-public class BezierDragView extends BaseCustomView {
+public class BezierDragView extends View {
+    private PointF mFixPoint;
+    private PointF mDragPoint;
+
+    private Paint mPaint;
+    private float maxFixRadiusDistance = dip2px(getContext(), 10f);
+    private float mFixRadius = dip2px(getContext(), 10f);
+    private float mMinFixRadius = dip2px(getContext(), 2);
+
+    private float mDraRadius = dip2px(getContext(), 10f);
 
     public BezierDragView(Context context) {
-        super(context);
+        this(context, null);
     }
 
-    @Override
-    public void initView(Context context, AttributeSet attrs) {
+    public BezierDragView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
+    public BezierDragView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context, attrs);
+    }
+
+
+    public void initView(Context context, AttributeSet attrs) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setDither(true);
+        mPaint.setColor(Color.RED);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                initPoint(event);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                updateDragPoint(event);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    private void updateDragPoint(MotionEvent event) {
+        mDragPoint.x = event.getX();
+        mDragPoint.y = event.getY();
+    }
+
+    private void initPoint(MotionEvent event) {
+        mFixPoint = new PointF(event.getX(), event.getY());
+        mDragPoint = new PointF(event.getX(), event.getY());
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mFixPoint == null || mDragPoint == null) {
+            return;
+        }
+        canvas.drawCircle(mDragPoint.x, mDragPoint.y, mDraRadius, mPaint);
+
+
+        float pDistance = getPointDistance();
+        mFixRadius = maxFixRadiusDistance - pDistance / 14;
+
+        if (mFixRadius > mMinFixRadius) {
+            canvas.drawCircle(mFixPoint.x, mFixPoint.y, mFixRadius, mPaint);
+        }
+    }
+
+    private float getPointDistance() {
+        return (float) Math.sqrt(
+                (mFixPoint.x - mDragPoint.x) * (mFixPoint.x - mDragPoint.x) +
+                        (mFixPoint.y - mDragPoint.y) * (mFixPoint.y - mDragPoint.y)
+        );
+    }
+
+    private int dip2px(Context context, float dpValue) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * density + 0.5f);
     }
 }
