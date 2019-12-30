5f9336fa0debc2575b00cf7120b1cb51b014f079
wangfayouy <f_y_wang6@163.com>
2019-12-30 10:17:23 +0800

    update

--- app/src/main/java/com/gavin/asmdemo/aop/AOPActivity.kt
+++ app/src/main/java/com/gavin/asmdemo/aop/AOPActivity.kt
@@ -2,9 +2,13 @@ package com.gavin.asmdemo.aop
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import com.gavin.asmdemo.R
+import me.ele.lancet.base.Origin
+import me.ele.lancet.base.annotations.Proxy
+import me.ele.lancet.base.annotations.TargetClass
 
 class AOPActivity : AppCompatActivity() {
 
@@ -12,7 +16,6 @@ class AOPActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aop)
     }
-
 
     @VipCheck("svip")
     fun svip(view: View) {
