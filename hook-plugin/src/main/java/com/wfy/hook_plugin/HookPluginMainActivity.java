f7988764a974a47627a2badf067948ef57e04dba
wangfayouy <f_y_wang6@163.com>
2020-07-13 15:48:38 +0800

    update

---
+++ hook-plugin/src/main/java/com/wfy/hook_plugin/HookPluginMainActivity.java
@@ -0,0 +1,17 @@
+package com.wfy.hook_plugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+/**
+ * hook插件
+ */
+public class HookPluginMainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hook_plugin_main);
+    }
+}
---
+++ hook-pluginhost/src/main/java/com/wfy/hook_pluginhost/HookHostMainActivity.java
@@ -0,0 +1,15 @@
+package com.wfy.hook_pluginhost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class HookHostMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_main);
+    }
+}
