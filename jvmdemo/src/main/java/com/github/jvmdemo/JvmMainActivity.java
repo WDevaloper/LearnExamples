a4f6f49d9e77928a5d75587b66dedd07862d4cce
wangfayouy <f_y_wang6@163.com>
2020-09-25 17:29:52 +0800

    update

--- jvmdemo/src/main/java/com/github/jvmdemo/JvmMainActivity.java
+++ jvmdemo/src/main/java/com/github/jvmdemo/JvmMainActivity.java
@@ -5,8 +5,10 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -30,10 +32,23 @@ public class JvmMainActivity extends AppCompatActivity {
         // 这两个对象的shadow$_klass_是一样的，即：Class在内存中值存在一份
         // shadow$_klass_ 就是Object的字段
         // 而Class 中又有classLoader字段表示，这个Class是由哪个classLoader加载的
+
+        // oopDesc中包含两个数据成员：_mark 和 _metadata。
+        // 其中markOop类型的_mark对象指的是前面讲到的Mark World。_metadata是一个共用体，其中_klass是普通指针，
+        // _compressed_klass是压缩类指针，它们就是前面讲到的元数据指针，这两个指针都指向instanceKlass对象，它用来描述对象的具体类型。
+
+        // InstanceKlass继承klass， InstanceKlass是Java Class 的VM级别表示。
+        // 它包含运行时在Class上所需的所有信息。
         JvmTest jvmTest = new JvmTest();
         jvmTest.test(1000);
         JvmTest jvmTest1 = new JvmTest();
         jvmTest1.test(400);
+
+        try {
+            OopKlassModel.main(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
---
+++ jvmdemo/src/main/java/com/github/jvmdemo/OopKlassModel.java
@@ -0,0 +1,31 @@
+package com.github.jvmdemo;
+
+import java.util.Arrays;
+
+/**
+ * Java对象模型  oop-klass
+ * <p>
+ * <p>
+ * Java中类基本都对应着Jvm中C++类或结构体
+ * <p>
+ * oppKlassDesc  ---->  instanceKlass
+ */
+public class OopKlassModel {
+    public static int a = 1;
+    private int b;
+
+    public OopKlassModel(int b) {
+        this.b = b;
+    }
+
+    public static void main(String[] args) {
+        int c = 10;
+        OopKlassModel modelA = new OopKlassModel(2);
+        OopKlassModel modelB = new OopKlassModel(3);
+        System.out.println("args = " + Arrays.deepToString(args));
+    }
+
+    public void test() {
+        System.out.println("test");
+    }
+}
